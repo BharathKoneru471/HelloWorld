fa55950b2cebf840a7826a36dbaa8711efff3d64
BharatKoneru471 <konerubharath999@gmail.com>
2020-04-07 01:26:24 +0530

    adding HelloWorld program

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld{
+	public static void main(String[] args){
+		int i=0;
+		for(i=0; i<5;i++){
+		System.out.println("HelloWorld");
+		}
+		System.out.println("HelloWorld is printed "+i+" times");
+	}
+	
+}
